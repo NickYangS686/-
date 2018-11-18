261b45ea92f5b858a94684933c1f9bb52dbd8919
NickYangS686 <335684481@qq.com>
2018-11-18 21:47:54 +0800

    second完成

---
+++ public/back/js/second.js
@@ -0,0 +1,135 @@
+$(function(){
+
+  var currentPage = 1;//当前页
+  var pageSize = 5;//一页5条
+  // 一进入页面，进行ajax请求，请求数据渲染当前页面
+  render();
+   function render(){
+    $.ajax({
+      type:"get",
+      dataType:"json",
+      url:"/category/querySecondCategoryPaging",
+      data:{
+        page:currentPage,
+        pageSize:pageSize,
+      },
+      success:function( info ){
+        console.log(info);
+        var htmlStr = template("tmp",info);
+        $('tbody').html( htmlStr );
+
+        // ajax请求完后就进行分页初始化
+        $('#paginator').bootstrapPaginator({
+          bootstrapMajorVersion: 3, // 版本号
+          totalPages: Math.ceil( info.total / info.size ),
+          currentPage: info.page,
+          onPageClicked: function( a, b, c, page ) {
+            // 更新当前页
+            currentPage = page;
+            // 重新渲染
+            render();
+          }
+        })
+      }
+    });
+   }
+
+  // 2. 点击添加按钮，显示模态框
+  $('#addBtn').click(function(){
+    $('#addModal').modal('show');
+
+    // 点击按钮，下拉框就开始请求后台数据
+    // 获取下拉菜单的列表数据
+    $.ajax({
+      type:"get",
+      url:"/category/queryTopCategoryPaging",
+      data:{
+        page:1,
+        pageSize:100
+      },
+      dataType:"json",
+      success:function( info ){
+        console.log(info);
+        var htmlStr = template("tpl",info);
+        $('.dropdown-menu').html( htmlStr );
+      }
+    });
+
+  })
+  //3.给下拉菜单的所有a 添加点击事件，通过事件委托注册
+  $('.dropdown-menu').on("click","a",function(){
+    //获取到a里面的文本
+    var txt = $(this).text();
+    //将文本设置给按钮
+    $('#dropdownText').text( txt );
+    // 获取id给设置好的input
+    var id = $(this).data("id");
+    $('[name="categoryId"]').val( id );
+     // 手动将 name="categoryId" 的校验状态, 改成 VALID 校验成功
+    $('#form').data("bootstrapValidator").updateStatus("categoryId", "VALID")
+  })
+
+  //4.进行文件上传初始化
+  $('#fileupload').fileupload({
+    dataType:"json",
+    // 表示文件上传完成的回调函数
+    done:function( e , data){
+      console.log( data );
+      // 后台返回的结果
+      var result = data.result;
+      // 获取文件上传的地址
+      var picUrl = result.picAddr;
+      // 设置给img的src
+      $('#imgBox img').attr("src",picUrl);
+      // 将src路径实时设置给input
+      $('[name="brandLogo"]').val( picUrl );
+       // 将 name="brandLogo" 的校验状态, 改成成功
+       $('#form').data("bootstrapValidator").updateStatus("brandLogo", "VALID");
+      }
+  });
+
+  // 5. 配置表单校验
+  $('#form').bootstrapValidator({
+
+    // 配置排序项, 默认会对隐藏域进行排除, 我们需要对隐藏域进行校验
+    excluded: [],
+
+    // 配置校验图标
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',    // 校验成功
+      invalid: 'glyphicon glyphicon-remove',  // 校验失败
+      validating: 'glyphicon glyphicon-refresh'  // 校验中
+    },
+
+    // 校验字段
+    fields: {
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: "请选择一级分类"
+          }
+        }
+      },
+
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: "请输入二级分类名称"
+          }
+        }
+      },
+
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请选择图片"
+          }
+        }
+      }
+    }
+  })
+
+
+
+
+})
