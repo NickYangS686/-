43d832ac1501c106998e87e19d37b4ea5d1bf398
NickYangS686 <335684481@qq.com>
2018-11-19 09:12:04 +0800

    product静态页面完成

---
+++ public/back/js/product.js
@@ -0,0 +1,15 @@
+// $(function(){
+
+//   var currentPage = 1;//当前页
+//   var pageSize = 5;//每页条数
+//   var picArr = [];//存储所有用于上传的对象
+
+//   //1.一进入页面，发送请求，渲染页面
+//   render();
+//   function render(){
+//     $.ajax({
+
+//     })
+//   }
+
+// })
